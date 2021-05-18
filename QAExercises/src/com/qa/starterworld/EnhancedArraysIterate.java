1db6f44e7c67522061afa8a0557d63828ca432dc
Sean Jackson <PowerUpSoup@Gmail.com>
2021-05-18 06:31:22 -0700

    SJ: Enhanced Array exercises

---
+++ QAExercises/src/com/qa/starterworld/EnhancedArraysIterate.java
@@ -0,0 +1,13 @@
+package com.qa.starterworld;
+
+public class EnhancedArraysIterate {
+
+	public static String[] strArr = { "one", "two", "three", "four", "five", "six" };
+	
+	public static void main(String[] args) {
+		for ( String str : strArr ) {
+			System.out.println(str);
+		}
+	}
+
+}
