0a99424e8e6b81f2d4f958e5cfea71e716bc3c8d
Sean Jackson <PowerUpSoup@Gmail.com>
2021-05-19 14:28:17 -0700

    SJ: adding oop exercises

---
+++ QAExercises/src/com/qa/starterworld/Book.java
@@ -0,0 +1,13 @@
+package com.qa.starterworld;
+
+public class Book {
+	public String title;
+	public String author;
+	
+	// Constructor
+	public Book(String title, String author) {
+		this.title = title;
+		this.author = author;
+	}
+
+}
